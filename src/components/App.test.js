e8a322f08b0b0ab82923ee6154838e0a2b4acd49
Mr. Metelus <Mdmetelus@gmail.com>
2019-01-25 22:59:17 -0500

    add gift test passed

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -8,12 +8,18 @@ it('renders correctly', () => {
     expect(app).toMatchSnapshot();
 });
 
-it ('initializes the`state` with and empty list of gifts', () => {
+it('initializes the`state` with and empty list of gifts', () => {
     expect(app.state().gifts).toEqual([]);
 });
 
-it ('adds a new gift to `state` when clicking the `add gift` button', () => {
+it('adds a new gift to `state` when clicking the `add gift` button', () => {
     app.find('.btn-add').simulate('click');
 
     expect(app.state().gifts).toEqual([{ id: 1}]);
+});
+
+it('adds a new gift to the rendered list when clicking the `add gift` button', () => {
+    app.find('.btn-add').simulate('click');
+    expect(app.find('.gift-list').children().length).toEqual(2);
+
 });
