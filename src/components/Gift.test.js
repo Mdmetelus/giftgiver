67a3df17b19a99d50552ee6464634e3f1ea912df
Mr. Metelus <Mdmetelus@gmail.com>
2019-01-27 12:33:59 -0500

    gift class

--- src/components/Gift.test.js
+++ src/components/Gift.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Gift from './Gift';
-import { isMainThread } from 'worker_threads';
+// import { isMainThread } from 'worker_threads';
 
 describe('Gift', () => {
     const gift = shallow(<Gift />);
