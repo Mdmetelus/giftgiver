997c2deb7f35118c0647e0bbbadf49197d9cb83c
Mr. Metelus <Mdmetelus@gmail.com>
2019-01-26 23:36:45 -0500

    ...

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -21,15 +21,21 @@ describe('App', () => {
 
         });
 
-        it('adds a new gift to `state` when clicking the `add gift` button', () => {
+        afterEach(() => {
+            app.setState({ gifts: [] });
+
+        });
+
+        it('adds a new gift to `state` ', () => {
             // app.find('.btn-add').simulate('click');
         
             expect(app.state().gifts).toEqual([{ id: 1}]);
         });
         
-        it('adds a new gift to the rendered list when clicking the `add gift` button', () => {
+        it('adds a new gift to the rendered list', () => {
             // app.find('.btn-add').simulate('click');
-            expect(app.find('.gift-list').children().length).toEqual(2);
+
+            expect(app.find('.gift-list').children().length).toEqual(1);
         
         });
     });
